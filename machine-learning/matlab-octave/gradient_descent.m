49747c4a7e9439ce4bbdb821efe419106b8750c7
Lyn Stanford <lynstanford@users.noreply.github.com>
2021-11-13 17:12:10 -0500

    Gradient Descent in Matlab
    MATLAB & Octave file showing gradient descent using basic linear function with 2 variables.

---
+++ machine-learning/matlab-octave/gradient_descent.m
@@ -0,0 +1,12 @@
+% Based on a differentiable function (no vertical tangents)
+y = (x + 5)^2;
+
+% Step 1 - Let's start from a random point
+x = -3;
+
+% Find y:
+x = sqrt(y - 5);
+
+% Then find the gradient of the function, or 1st order derivative w.r.t. the function
+diff(y) / diff(x) = 2 * (x + 5);
+
