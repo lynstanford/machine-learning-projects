732ea6591e2780c77e7ea03413b4be43f8656b96
Lyn Stanford <lynstanford@users.noreply.github.com>
2021-11-14 20:52:18 -0500

    Comment out x and y variables and assign value to learning_rate variable

--- machine-learning/matlab-octave/gradient_descent.m
+++ machine-learning/matlab-octave/gradient_descent.m
@@ -1,6 +1,6 @@
 % Define variable x as a vector
-x = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-y = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+% x = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+% y = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 % Based on a differentiable function (no vertical tangents)
 y = (x + 5)^2;
@@ -17,9 +17,9 @@ x = sqrt(y - 5);
 % Then find the gradient of the function, or 1st order derivative w.r.t. the function
 diff(y) / diff(x) = 2 * (x + 5);
 
-''' Step 2 - Move in the direction of the negative of the gradient (Descent)
-But how much do we move by? For that, we define a learning rate. Learning Rate = 0.01
-'''
+% Step 2 - Move in the direction of the negative of the gradient (Descent)
+% But how much do we move by? For that, we define a learning rate.
+learning_rate = 0.01;
 
 % Step 3 - perform 2 iterations of gradient descent
 % i) Initialize Parameters:
@@ -41,3 +41,4 @@ x2;
 % A better way to represent this
 x_new = x+=1;
 x_new = x - learning_rate * gradient;
+
