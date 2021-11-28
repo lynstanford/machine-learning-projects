730eac2155b899ab7712dbd3c4af180b4ef1ad0f
Lyn Stanford <lynstanford@users.noreply.github.com>
2021-11-28 14:34:35 -0500

    Update grad_desc_statquest.m
    Linear equation or relationship between dependent and independent variables and their prediction.

---
+++ machine-learning/matlab-octave/grad_desc_statquest.m
@@ -0,0 +1,16 @@
+# GRADIENT DESCENT - STEP BY STEP
+# Step 1: Calculate the 1st residual
+x = 0.5;
+y = 1.4;
+
+gradient = delta(y) / delta(x);
+gradient
+
+# Using the following equation for linear regression:
+y = mx + b;
+y = predicted value;
+m = slope, y / x = 0.64;
+x = actual value, 0.5;
+b = intercept, 0;
+y_predicted = 0 + 0.64 * 0.5
+y_predicted = 0.32
