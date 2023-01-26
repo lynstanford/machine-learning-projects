5c8c14c3b85bb760c02bf32f2d9384492c4ec13d
Lyn Stanford <lynstanford@users.noreply.github.com>
2023-01-26 08:12:01 -0500

    Changes to SQLite Connection File
    Run the connection file. Run logistic regression file.

--- data-engineering/sqlite_connection.py
+++ data-engineering/sqlite_connection.py
@@ -8,9 +8,9 @@ c = conn.cursor()
 
 # create a Table
 c.execute("""CREATE TABLE customers(
-        first_name DATATYPE,
-        last_name DATATYPE,
-        email DATATYPE
+        first_name TEXT,
+        last_name TEXT,
+        email TEXT
 )""")
 
 # NULL
@@ -19,3 +19,10 @@ c.execute("""CREATE TABLE customers(
 # TEXT
 # BLOB
 
+# commit our connection command
+conn.commit()
+
+# close our connection
+conn.close()
+
+# save this file and run it in the bash
