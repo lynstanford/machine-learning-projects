fef843a31073973a2765e85b6e1c4af8abc34207
Lyn Stanford <lynstanford@users.noreply.github.com>
2023-04-15 20:25:34 -0400

    Update

--- data-engineering/sqlite3_11.py
+++ data-engineering/sqlite3_11.py
@@ -9,9 +9,14 @@ db = sqlite3.connect('books.db')
 # read data from SQL to pandas dataframe
 data = pd.read_sql_query('SELECT * FROM books;', db)
 
-new_row = {'id':'12','author':'P.G. Wodehouse','title':'Luck of the Bodkins','price':6.49}
+new_row = {
+    'id': '12',
+    'author': 'P.G. Wodehouse',
+    'title': 'Luck of the Bodkins',
+    'price': 6.49
+}
 
-data=data.append(new_row, ignore_index=True)
+data = data.append(new_row, ignore_index=True)
 
 print(data)
 
