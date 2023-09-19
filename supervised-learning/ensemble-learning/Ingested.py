36a701ae204140845749f07c9afa8f496ae9f2dc
Lyn Stanford <lynstanford@users.noreply.github.com>
2023-09-19 19:51:25 -0400

    Update Ingested.py

---
+++ supervised-learning/ensemble-learning/Ingested.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+import xgboost as xgb
+from sklearn.linear_model import LogisticRegression
+
+# Split and train the data
+from sklearn.model_selection import train_test_split
+
+# I will construct a pipeline containing my chosen models
+from sklearn.pipeline import Pipeline
+
+# I need to score predicted versus actual values
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score, make_scorer, confusion_matrix, ConfusionMatrixDisplay
+
+# I need to cross-validate and evaluate results
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import GridSearchCV
+
+telco_churn = pd.read_csv("C:/Users/lynst/Documents/Datasets/Kaggle/Jack Chang/telco_churn.csv")
+telco_churn.head()
