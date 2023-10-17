d58c14b9a77a0209167ef613619687e045fc77e3
Lyn Stanford <lynstanford@users.noreply.github.com>
2023-10-16 22:59:56 -0400

    Import libraries
    Import libraries and set up source url. Create a database name and store the object in this variable.

---
+++ data-engineering/web-scraping/webscraping_movies_update.py
@@ -0,0 +1,8 @@
+import requests
+import sqlite3
+import pandas as pd
+from bs4 import BeautifulSoup
+
+url = 'https://web.archive.org/web/20230902185655/https://en.everybodywiki.com/100_Most_Highly-Ranked_Films'
+
+db_name = 'Movies.db'
