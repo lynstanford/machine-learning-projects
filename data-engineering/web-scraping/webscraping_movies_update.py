a20eac29588ed281eda701b0c7a84851d1ae9b03
Lyn Stanford <lynstanford@users.noreply.github.com>
2023-10-16 23:01:25 -0400

    Top 25 Movies Only
    Change name to Top_25 movies.

--- data-engineering/web-scraping/webscraping_movies_update.py
+++ data-engineering/web-scraping/webscraping_movies_update.py
@@ -6,3 +6,6 @@ from bs4 import BeautifulSoup
 url = 'https://web.archive.org/web/20230902185655/https://en.everybodywiki.com/100_Most_Highly-Ranked_Films'
 
 db_name = 'Movies.db'
+
+table_name = 'Top_25'
+
